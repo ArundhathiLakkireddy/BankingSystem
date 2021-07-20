8b0ba8d922788cfd253a3b2c748ef55758213f58
ArundhathiLakkireddy <87596283+ArundhathiLakkireddy@users.noreply.github.com>
2021-07-20 10:30:35 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<HTML>
+<head>
+	 <meta name="author" content="Vinit Shahdeo">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="format-detection" content="telephone=no">
+      <link rel="stylesheet" href="style.css">
+      <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Caveat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Caveat|Cookie&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Delius&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" crossorigin="anonymous">
+	<center><title>GSP Bank</title></center>
+	<link rel = "stylesheet" type = "text/css" href = "buttons.css">
+</head>
+
+<!--<body style="background:linear-gradient(to bottom ,black,	#191970,	#000080,#00008B	,#4169E1,blue);background-attachment: fixed;background-repeat: no-repeat;background-size: cover;">-->
+
+<body style="background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,121,114,1) 35%, rgba(0,212,255,1) 100%);background-attachment: fixed;background-repeat: no-repeat;background-size: cover;"	
+
+<link rel = "stylesheet" type = "text/css" href = "style.css">
+
+
+	<div class="one" style=" background-color:whitesmoke;
+   height: :50px;
+   width:100%;">
+		<table width="1330">
+            		
+			<table width="1200" align="center"  class = "t">
+                <tr>
+				<td style = "text-align:center"> <button class = "btn1"> Home </button></td> 	
+				<td style = "text-align:center"><a href="customer.php" target="frame"><button class = "btn2">View Customers</button></a></td>
+				<td style = "text-align:center"><a href="Transfermoney.php" target="frame"><button class = "btn2">Transfer Money</button></a></td>
+				<td style = "text-align:center"><a href="transactionhistory.php" target="frame"><button class = "btn2">View Transaction History</button></a></td>
+                </tr>
+            </table>
+        </table>
+	</div>
+	<div class="two">Welcome to</div>
+	<h1 >
+	
+<span style="color: #FFFFFF;">T</span>
+<span style="color: #FFFFFF;">H</span>
+<span style="color: #FFFFFF;">E</span>
+<span style="color: #4B0082;">G</span>
+<span style="color: #4B0082;">S</span>
+<span style="color: #4B0082;">P</span>
+<span style="color: #FFFFFF;">B</span>
+<span style="color: #FFFFFF;">A</span>
+<span style="color: #FFFFFF;">N</span>
+<span style="color: #FFFFFF;">K</span>
+</h1>
+	<div class="three" ><br> </div> 
+</div>
+<div class="five">Designed for THE SPARKS FOUNDATION INTERNSHIP PROGRAM<br> For any queries contact:123456789
+</div>
+</div>
+</body>
+</html>
